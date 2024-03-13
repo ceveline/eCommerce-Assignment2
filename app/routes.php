534c37165694beac7feb472d572ef1a7361c21fe
ceveline <cevelinevangelista@gmail.com>
2024-03-13 19:08:52 -0400

    \Profile\index is working also
    shows the data from the database (profile table)

--- app/routes.php
+++ app/routes.php
@@ -2,5 +2,6 @@
 //defined a few routes "url"=>"controller,method" -> connected to the App.php file in the core folder
 $this->addRoute('User/register','User,register');
 $this->addRoute('User/login','User,login');
+$this->addRoute('User/logout','User,logout');
 $this->addRoute('Profile/create','Profile,create');
 $this->addRoute('Profile/index','Profile,index');
--- app/views/Profile/index.php
+++ app/views/Profile/index.php
@@ -1,26 +1,21 @@
 <html>
 <head>
-	<title><?= $name ?> view</title>
+	<title>Profile</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
 	<div class='container'>
-		<form method='post' action=''>
-			<div class="form-group">
-				<label>First name:<input type="text" class="form-control" name="first_name" placeholder="<?=$data->first_name?>" /></label>
-			</div>
-			<div class="form-group">
-				<label>Middle name:<input type="text" class="form-control" name="first_name" placeholder="Jon" /></label>
-			</div>
-			<div class="form-group">
-				<label>Last name:<input type="text" class="form-control" name="last_name" placeholder="Doe" /></label>
-			</div>
-			<div class="form-group">
-				<input type="submit" name="action" value="Record my profile" /> 
-				<!-- <a href='/Profile/index'>Cancel</a> (should not be here) -->
-			</div>
-		</form>
+		<h1>My profile</h1>
+			<dl>
+			<dt>First name:</dt>
+			<dd><?= $data->first_name ?></dd>
+			<dt>Middle name:</dt>
+			<dd><?= $data->middle_name ?></dd>
+			<dt>Last name:</dt>
+			<dd><?= $data->last_name ?></dd>
+			</dl>
+			<a href='/Profile/modify'>Modify my profile</a> | <a href='/User/logout'>Logout</a>
 	</div>
 </body>
 </html>
