1ad2a9612bf5f0407538ca9638e4ae0cc8b49e45
Ceveline <74314874+ceveline@users.noreply.github.com>
2024-03-18 13:20:34 -0400

    updated Publication/create (styling)

--- app/views/Publication/create.php
+++ app/views/Publication/create.php
@@ -4,13 +4,18 @@
     <title>Create Publication</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <style>
+        #title {
+            display: grid;
+        }
+    </style>
 </head>
 <body>
     <div class='container'>
         <form id="main" method='post' action=''>
-            <div class="form-group">
+            <div class="form-group" id="title">
                 <label>Title:<input type="text" class="form-control" name="publication_title" /></label>
-            </div>
+            </div><br>
 
             <div class="form-group">
                 <textarea id="publication_text" form="main" class="form-control" name="publication_text" rows="6" maxlength="255" placeholder="Type your content here"></textarea>
@@ -19,7 +24,7 @@
             <div class="radioBtns" require>
                 <input type="radio" name="publication_status" value="0"><label>Private</label></input>
                 <input type="radio" name="publication_status" value="1"><label>Public</label></input>
-            </div>
+            </div><br>
             <div class="form-group">
                 <input id="btn" type="submit" name="action" value="Publish" onclick="return validateForm();" />
             </div>
